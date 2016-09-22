d44e5ec1d4e0bbcf61cef0da898e96077fad5874
Галин Киров <g_9635@abv.bg>
2016-09-22 14:07:09 +0300

    Модул 08, Видео 09 - Четене от XML файл

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -13,13 +13,15 @@ namespace IOTextFiles
 
 		public string getPath ()
 		{
-			string _path = System.IO.Path.Combine ( AppDomain.CurrentDomain.BaseDirectory, "test.txt" );
+			//Програ\Setting\settings.txt
+			string _path = System.IO.Path.Combine ( AppDomain.CurrentDomain.BaseDirectory, "Settings" );
+			_path = System.IO.Path.Combine ( _path, "settings.txt");
 
 			//Други видове директории
-			string _user = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData);
-			Console.WriteLine (_user);
-			string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);
-			Console.WriteLine (_desktop);
+			//string _user = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData);
+			//Console.WriteLine (_user);
+			//string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);
+			//Console.WriteLine (_desktop);
 
 			return _path;
 		}
@@ -46,6 +48,8 @@ namespace IOTextFiles
 		{
 			try 
 			{
+				iniSettings ();
+
 				string _temp = "", _filePath = getPath ();
 
 				if ( System.IO.File.Exists ( _filePath )) //Проверка дали пътят е валиден
@@ -58,16 +62,49 @@ namespace IOTextFiles
 					{
 						_stable.stable[i] = _table[i];
 					}
+
 				}else {
-					Console.WriteLine ("Не е намерен такъв път.");
+					
 					return false;
 				}
+
+				System.Diagnostics.Process.Start ( _filePath);
 
 				return true;
 			}catch {
 			}
 
 			return false;
+		}
+
+
+		private void iniSettings ()
+		{
+			try
+			{
+				bool _fileExist = System.IO.File.Exists (getPath ());
+
+				if ( !_fileExist )
+				{
+					string _directory = System.IO.Path.GetDirectoryName (getPath ());
+
+					//Застраховаме се, че директорията съществува
+					if ( !System.IO.Directory.Exists (_directory))
+					{
+						System.IO.Directory.CreateDirectory ( _directory);
+					}
+
+					//Запаметяваме съдържанието на файла
+					save ();
+
+				}
+
+				
+			}catch{
+			}
+
+
+
 		}
 
 	}
--- IOTesting/IOXMLFiles/Program.cs
+++ IOTesting/IOXMLFiles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace IOXMLFiles
 {
@@ -6,7 +8,68 @@ namespace IOXMLFiles
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			string value = "", property = "";
+
+			string path	= System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.xml");
+
+			//Прочитане на xml файл
+			using (XmlReader reader = XmlReader.Create (path)) {
+
+				while (reader.Read ()) {
+					switch (reader.Name) {
+
+					case "row":
+						property = reader ["property"];
+						value = reader.ReadInnerXml ();
+
+						Console.WriteLine ("Value = " + value);
+						Console.WriteLine ("Property = " + property);
+
+						break;
+
+					case "SimpleRow":
+						//property = reader ["property"];
+						value = reader.ReadInnerXml ();
+
+						Console.WriteLine ("Value = " + value);
+						//Console.WriteLine ("Property = " + property);
+
+						break;
+					}
+				
+				}
+			}
+
+
+			Console.ReadKey ();
+
+			//Запис на xml файл
+
+//			string path	= System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "test.xml");
+//
+//			using (XmlWriter writer = XmlWriter.Create (path)) {
+//
+//				writer.WriteStartDocument ();
+//				writer.WriteStartElement ("Settings");
+//
+//				//Съдържание на файла
+//				writer.WriteStartElement ("row");
+//
+//				writer.WriteAttributeString ( "property", property); 	//<row propetry=" ..."> ... </row>
+//				writer.WriteString (value); //<row> value </row>
+//
+//				writer.WriteEndElement ();
+//
+//
+//				writer.WriteEndElement ();
+//				writer.WriteEndDocument ();
+//
+//			}
+//
+//			XDocument document = XDocument.Load ( path );
+//			document.Save (path);
+//
+//			System.Diagnostics.Process.Start (path);
 		}
 	}
 }
